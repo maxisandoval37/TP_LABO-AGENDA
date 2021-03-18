d4e6d19b5492c89d866823a3a8e8d5b24271c931
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-03-18 17:42:42 -0300

    script sql actualizado

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -5,5 +5,13 @@ CREATE TABLE `personas`
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
+  `Calle` varchar(20) NOT NULL,
+  `Altura` int(8) NOT NULL,
+  `Piso` int(4),
+  `Departamento` int(4),
+  `Localidad` varchar(20),
+  `Email` varchar(30),
+  `Etiqueta` varchar(20),
   PRIMARY KEY (`idPersona`)
 );
+
