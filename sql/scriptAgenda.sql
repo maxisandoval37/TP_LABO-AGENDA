c2ff998c1f4ea5ec50d24989e458edc442ab201d
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-03-22 16:53:58 -0300

    sql actualizado con el obj etiqueta

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -1,18 +1,28 @@
 CREATE DATABASE `agenda`;
 USE agenda;
+
+CREATE TABLE `etiquetas`(
+  idEtiqueta int(11) NOT NULL AUTO_INCREMENT,
+  tipoEtiqueta varchar(45) NOT NULL,
+
+  PRIMARY KEY (idEtiqueta)
+);
+
 CREATE TABLE `personas`
 (
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  `Calle` varchar(20) NOT NULL,
-  `Altura` int(8) NOT NULL,
+  `Calle` varchar(20),
+  `Altura` int(8),
   `Piso` int(4),
   `Departamento` int(4),
-  `Localidad` varchar(20),
+  `Localidad` varchar(20) NOT NULL,
   `Email` varchar(30),
-  `Etiqueta` varchar(20),
-  PRIMARY KEY (`idPersona`)
+  `idEtiqueta` int(11) NOT NULL,
+
+  PRIMARY KEY (`idPersona`),
+  FOREIGN KEY (idEtiqueta) REFERENCES Etiquetas(idEtiqueta)
 );
 
 CREATE USER 'montenegro_sandoval'@'localhost' IDENTIFIED BY '1234';
--- src/main/java/persistencia/dao/mysql/DAOSQLFactory.java
+++ src/main/java/persistencia/dao/mysql/DAOSQLFactory.java
@@ -4,6 +4,7 @@ import persistencia.dao.interfaz.DAOAbstractFactory;
 import persistencia.dao.interfaz.PersonaDAO;
 
 public class DAOSQLFactory implements DAOAbstractFactory {
+	
 	public PersonaDAO createPersonaDAO() {
 		return new PersonaDAOSQL();
 	}
