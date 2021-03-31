43d2d239ed3bf87970d9e02d918bc20b896fa473
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-03-31 15:09:58 -0300

    fixed error no genera reporte jasper

--- src/main/java/presentacion/reportes/ReporteAgenda.java
+++ src/main/java/presentacion/reportes/ReporteAgenda.java
@@ -6,9 +6,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.log4j.Logger;
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -18,34 +16,28 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 import dto.PersonaDTO;
 
-public class ReporteAgenda
-{
+public class ReporteAgenda {
 	private JasperReport reporte;
 	private JasperViewer reporteViewer;
-	private JasperPrint	reporteLleno;
+	private JasperPrint reporteLleno;
 	private Logger log = Logger.getLogger(ReporteAgenda.class);
-	//Recibe la lista de personas para armar el reporte
-    public ReporteAgenda(List<PersonaDTO> personas)
-    {
-    	//Hardcodeado
+
+	public ReporteAgenda(List<PersonaDTO> personas) {
+		// Hardcodeado
 		Map<String, Object> parametersMap = new HashMap<String, Object>();
-		parametersMap.put("Fecha", new SimpleDateFormat("dd/MM/yyyy").format(new Date()));		
-    	try		{
-			this.reporte = (JasperReport) JRLoader.loadObjectFromFile( "reportes" + File.separator + "ReporteAgenda.jasper" );
-			this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametersMap, 
-					new JRBeanCollectionDataSource(personas));
-    		log.info("Se cargó correctamente el reporte");
-		}
-		catch( JRException ex ) 
-		{
+		parametersMap.put("Fecha", new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+		try {
+			this.reporte = (JasperReport) JRLoader.loadObjectFromFile("reportes" + File.separator + "ReporteAgenda.jasper");
+			this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametersMap,new JRBeanCollectionDataSource(personas));
+			log.info("Se cargó correctamente el reporte");
+		} catch (JRException ex) {
 			log.error("Ocurrió un error mientras se cargaba el archivo ReporteAgenda.Jasper", ex);
 		}
-    }       
-    
-    public void mostrar()
-	{
-		this.reporteViewer = new JasperViewer(this.reporteLleno,false);
+	}
+
+	public void mostrar() {
+		this.reporteViewer = new JasperViewer(this.reporteLleno, false);
 		this.reporteViewer.setVisible(true);
 	}
-   
-}	
+
+}
